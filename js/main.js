1ee6ecf26c2a14c1a851ef11725d837bcd0d944b
Zeran Lu <zeranlu0326@gmail.com>
2025-03-05 17:06:05 -0500

    final touches
    this is a final touches commit, cleaning up my code and making it look presentable

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@
 let theThumbnails = document.querySelectorAll('#buttonHolder img'),
     gameBoard = document.querySelector('.puzzle-board'),
     originalBox = document.querySelector('.puzzle-pieces')
-
-    originalBox = document.querySelector('.puzzle-pieces')
-
     pzlPieces = document.querySelectorAll('.puzzle-pieces img'),
     dropZone = document.querySelectorAll('.drop-zone'),
     resetPieces = document.querySelector('.reset-pieces');
@@ -13,13 +10,6 @@ let theThumbnails = document.querySelectorAll('#buttonHolder img'),
     topRightPuzzlePiece = document.querySelector('#topRight');
     bottomLeftPuzzlePiece = document.querySelector('#bottomLeft');
     bottomRightPuzzlePiece = document.querySelector('#bottomRight');
-
-    topLeftPuzzlePiece = document.querySelector('#topLeft');
-    topRightPuzzlePiece = document.querySelector('#topRight');
-    bottomLeftPuzzlePiece = document.querySelector('#bottomLeft');
-    bottomRightPuzzlePiece = document.querySelector('#bottomRight');
-
-    
 // functions
 function changeImageSet() {
     gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
@@ -28,14 +18,6 @@ function changeImageSet() {
     topRightPuzzlePiece.setAttribute("src", `images/topRight${this.dataset.bgref}.jpg`);
     bottomLeftPuzzlePiece.setAttribute("src", `images/bottomLeft${this.dataset.bgref}.jpg`);
     bottomRightPuzzlePiece.setAttribute("src", `images/bottomRight${this.dataset.bgref}.jpg`);
-
-    topLeftPuzzlePiece.setAttribute("src", `images/topLeft${this.dataset.bgref}.jpg`);
-    topRightPuzzlePiece.setAttribute("src", `images/topRight${this.dataset.bgref}.jpg`);
-    bottomLeftPuzzlePiece.setAttribute("src", `images/bottomLeft${this.dataset.bgref}.jpg`);
-    bottomRightPuzzlePiece.setAttribute("src", `images/bottomRight${this.dataset.bgref}.jpg`);
-
-
-    pzlPieces.forEach(piece => originalBox.appendChild(piece));
 
     pzlPieces.forEach(piece => originalBox.appendChild(piece));
 }
@@ -51,24 +33,6 @@ function allowDrag(event) {
 function allowDragOver(event) {
     event.preventDefault();
     console.log('dragging over a drop zone');
-}
-
-function allowDrop(event) {
-    event.preventDefault();
-
-    let dropZone = this;
-
-    if (dropZone.children.length > 0) {
-        return;
-    }
-
-    //gets the dragged element's ID from the data transfer object (using 'draggedEl')
-    let droppedElId = event.dataTransfer.getData('draggedEl');    
-    
-    //gets the ACTUAL dragged element, using the ID
-    let droppedEl = document.querySelector(`#${droppedElId}`);
-    
-    dropZone.appendChild(droppedEl);
 }
 
 function allowDrop(event) {
